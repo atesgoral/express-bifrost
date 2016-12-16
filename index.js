fc88ed9d34cb8e0ca17d45adc9fa22adffb839fd
Ates Goral <ates.g@myplanet.com>
2016-12-16 10:48:26 -0500

    Added middleware

---
+++ index.js
@@ -0,0 +1,40 @@
+const defaults = {
+  bridgeReq: null,
+  bridgeRes: null,
+  bridgeErr: null
+};
+
+function bifrost(options) {
+  const bridge = Object.assign({}, defaults);
+
+  if (options instanceof Function) {
+    Object.assign(bridge, { req: options });
+  } else if (options instanceof Object) {
+    Object.assign(bridge, options);
+  }
+
+  return (req, res, next) => {
+    Promise
+      .resolve(bridge.req && bridge.req(req))
+      .then(data => {
+        if (bridge.res) {
+          bridge.res(res, data);
+        } else if (data !== undefined) {
+          res.send(data);
+        } else {
+          res.end();
+        }
+      })
+      .catch(error => {
+        if (bridge.err) {
+          bridge.err(res, next, error);
+        } else {
+          next(error);
+        }
+      });
+  };
+}
+
+bifrost.defaults = defaults;
+
+module.exports = bifrost;
